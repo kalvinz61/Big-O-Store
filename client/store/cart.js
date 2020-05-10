a359df0641208bef4f57d41d3063bebc636468f1
Kalvin Zhao <kalvinz61@gmail.com>
2020-05-10 10:08:00 -0400

    add to cart works

--- client/store/cart.js
+++ client/store/cart.js
@@ -21,7 +21,6 @@ const _deleteProduct = product => ({
 export const loadCart = () => {
   return async dispatch => {
     const cart = (await axios.get('/api/cart')).data
-    console.log('dispatch load cart')
     dispatch(_loadCart(cart))
   }
 }
@@ -36,39 +35,21 @@ export const addToCart = product => {
 export const deleteProduct = product => {
   return async dispatch => {
     await axios.delete('/api/cartsproducts', {data: product})
-    console.log('should dispatch delete now', product) //does not get logged
     dispatch(_deleteProduct(product))
   }
 }
 const initialState = {}
-
 export default function(state = initialState, action) {
   switch (action.type) {
     case LOAD_CART:
       return action.cart
     case DELETE_CART_PRODUCT:
-      const newList = state.products.filter(product => {
-        return product.id !== action.productId
-      })
-      state.products = newList
-      return state
+      const newState = {...state}
+      newState.products = state.products.filter(
+        product => product.id !== action.product.productId
+      )
+      return newState
     default:
       return state
   }
 }
-
-// state = {
-//     createdAt: "2020-05-09T18:59:19.533Z",
-//     id: "2c6445b2-0255-43c5-acac-53427de05499",
-//     products: (8)[{ … }, { … }, { … }, { … }, { … }, { … }, { … }, { … }],
-//     updatedAt: "2020-05-09T18:59:19.533Z",
-//     userId: "45e16acb-5173-42d0-9d4a-027ffbaa76f5",
-// }
-
-// action.product = {
-//     cartId: "2c6445b2-0255-43c5-acac-53427de05499",
-//     createdAt: "2020-05-09T18:59:19.572Z",
-//     productId: "c508d5e3-db08-4d37-8bf7-110e4532b923",
-//     quantity: 1,
-//     updatedAt: "2020-05-09T18:59:19.572Z"
-// }
